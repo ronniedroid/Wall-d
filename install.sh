c170c1cc51302bcc953ba44b9223c871fca410fd
ronniedroid <ronni.n.yousif@gmail.com>
2020-05-04 09:10:18 +0300

    testing some stuff

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-Current=$(pwd)
-C=$(echo "$Current")
+OUTDIR="$HOME/.local/bin/"
+state1=$(exa -1 "$OUTDIR" | grep -E "Wall-d")
+state2=$(exa -1 "$OUTDIR" | grep -E "defaultwallpaper.sh")
+CurrentDIR=$(pwd)
+PCurrentDir=$(echo "$Current")
 
-ln -s $C/Wall-d $HOME/.local/bin/
-ln -s $C/defaultwallpaper.sh $HOME/.local/bin/
+if [ -z "$state1" && -z "$state2" ]; then
+    ln -s "$PCurrentDir"Wall-d "$OUTDIR"
+    ln -s "$PCurrentDir"defaultwallpaper.sh "$OUTDIR"
+else
+    rm "$OUTDIR"Wall-d && rm "$OUTDIR"defaultwallpaper.sh
+    ln -s "$PCurrentDir"Wall-d "$OUTDIR"
+    ln -s "$PCurrentDir"defaultwallpaper.sh "$OUTDIR"
+fi
+
 
